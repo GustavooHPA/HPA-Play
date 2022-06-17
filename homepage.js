8c42f8e6274443fd425a2d2d8378034e9ea703c3
GustavooHPA <100046745+GustavooHPA@users.noreply.github.com>
2022-06-17 18:54:49 -0300

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,11 @@
+function MsMarvel() {
+    console.log("CAPITÃ MARVEEEEEEEEEEEEL")
+}
+
+function Ratatouille() {
+    window.location.replace("./ratatouille.html")
+}
+
+function Totoro() {
+    window.location.replace("./totoro.html")
+}
